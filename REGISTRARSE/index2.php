918bf7b4c3627661b365f7c3e7bc7f8e13676836
Magali Aguilera <37684719+11112017@users.noreply.github.com>
2018-04-06 18:28:48 -0500

    Jimena
    Se agrego informacion en Index2.php, despues de haber iniciado sesion y lo de google maps

--- REGISTRARSE/index2.php
+++ REGISTRARSE/index2.php
@@ -178,6 +178,69 @@ include("include/menu.php");
 	<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
 	<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
 </div>
+			
+<h3>MODELO EDUCATIVO</h3>
+<P>Estamos instalados en el municipio de Juventino Rosas, Guanajuato,pertenecemos al Subsistema Nacional de Universidades Politécnicas.
+En la UPJR contribuimos a que los egresados de nivel medio superior tengan opciones de continuar su formación profesional, adecuada a sus
+intereses y a las necesidades de desarrollo estatal, regional y nacional, que les permitan participar ventajosamente en el mercado laboral,
+razón por lo que sólo contamos con carreras vanguardistas.</P>
+
+<P>Nuestro sistema educativo se enfatiza en la búsqueda permanente de nuevas formas de enseñanza-aprendizaje diseñadas con enfoques educativos 
+flexibles y centrados en el aprendizaje, que desarrollan en los estudiantes habilidades para aprender a lo largo de la vida. Contando con 
+programas educativos de buena calidad, diseñados con base en competencias, entendidas como los conocimientos, habilidades, destrezas y actitudes
+necesarias para que el egresado pueda integrarse a cualquier ambiente.</P>
+
+<div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="wrap-item text-justify">
+			 <div class="item-img">
+               <img src="images/MODELO.jpg">
+              </div>
+<br>
+<P>En la formación de los alumnos se pone especial atención en el trabajo en grupo y en el fortalecimiento de valores,
+como por ejemplo, la honestidad, el respeto a los demás, la responsabilidad, la libertad y la solidaridad, con el objeto 
+de lograr en ellos una formación integral que contribuya a conformar una sociedad más justa.</P>
+</br>
+
+ <p>Se enfatiza también el aprendizaje del idioma inglés, de manera que el nivel de conocimiento alcanzado por nuestros alumnos 
+al egresar les permita competir con ventaja en el ámbito laboral, de cara a las demandas de la creciente integración internacional.</p>	
+
+<h3>Estructura Curricular</h3>
+<div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="wrap-item text-justify">
+			 <div class="item-img">
+               <img src="images/2.png">
+              </div>
+			  
+
+<h3>¿COMO PUEDES INSCRIBIRTE?</h3>
+
+
+<p>>Para poder reinscribirte deberás seguir el siguiente procedimiento:</p>
+<br>
+<p>
+-Descargar el recibo para el pago de Inscripción, seleccionar la Universidad Politécnica de Juventino Rosas y el concepto de Inscripción. Se encuentra  localizado en la página https://pagosenlinea.guanajuato.gob.mx/public/servicios?tipoServicio=ODES </br>
+<br>
+-Descargar el recibo de pago del Seguro Escolar (los encuentras en la pestaña de Servicios Escolares, en el menú superior de la página web de la UPJR).</br>
+<br>
+-Realizar el pago en el banco: $892 pesos de Inscripción y $50 pesos del seguro escolar (Obligatorio).</br>
+<br>
+-Entregar el voucher en el Departamento de Recursos Financieros.</br>
+<br>
+-Presentar los recibos sellados por el Departamento de Recursos Financieros al Departamento de Control Escolar.</br>
+<br>
+-Realizar precarga de materias.</br></p>
+
+<h3>Si tienes dudas puedes acudir al departamento de Control Escolar o comunícate.</h3>
+<p>VÍA TELEFÓNICA:
+(412) 157-8000 Ext. 320
+
+01 800 76-58-757</p>
+
+<p> O MEDIANTE CORREO ELECTRÓNICO:
+<br>auxiliar_escolares@upjr.edu.mx</br>
+<br>tec_escolares@upjr.edu.mx</br>
+serv_escolares@upjr.edu.mx</p>
+
 <h3>Materias incluidas en el programa de aprendizaje de la carrera en Ingeniería en Redes y Telecomunicaciones</h3>
 <div class="row" style="text-align:center">
 			<div class="span2">
@@ -185,7 +248,7 @@ include("include/menu.php");
 					<h4>1er. Cuatrimestre</h4>
 					<a href="al.php"><small>Ver detalles</small></a>	
 				</div>
-			</div>
+			</div>			
 			
 
     
